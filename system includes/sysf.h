b79d90da310e3c212f12bb526d8253b5301ec40a
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-03 09:29:32 -0600

    Create sysf.h

---
+++ system includes/sysf.h
@@ -0,0 +1,15 @@
+#ifndef SYSF_H
+#define SYSF_H
+
+void * coid(const int * _COID); void * sysfmalloc(std::size_t _MALLOC);
+int * sysfrunf(const char * _RUN);
+//
+typedef if sysfif;
+typedef bool sysfboolean;
+//
+#define DEFINE_SYSF_IF(if)
+#define DEFINE_SYSF_BOOL(bool)
+//
+#define SYSF_WRN_MSG(!)(const char * wrn_nsg)()
+
+#endif
